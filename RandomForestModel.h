4e956a2b7310a41fe20a4f9b1a4b49a40c1b5beb
Alfan Risqi Wahyudi <54757609+alfan459@users.noreply.github.com>
2024-12-22 08:45:21 +0700

    Create RandomForestModel.h

---
+++ RandomForestModel.h
@@ -0,0 +1,30 @@
+#ifndef RandomForestModel_h
+#define RandomForestModel_h
+
+#include "Arduino.h"
+
+class RandomForestModel {
+  public:
+    // Constructor
+    RandomForestModel(float coefVolume, float coefWaktu, float intercept, float learningRate);
+
+    // Predict waktu based on volume and target delta_tds
+    float predictWaktu(float volume, float targetDeltaTDS);
+
+    // Predict delta_tds based on volume and waktu
+    float predictDeltaTDS(float volume, float waktu);
+
+    // Update coefficients using feedback
+    void updateCoefficients(float volume, float waktu, float actualDeltaTDS, float predictedDeltaTDS);
+
+    // Print current model parameters
+    void printModelParameters();
+
+  private:
+    float _coefVolume;   // Coefficient for volume
+    float _coefWaktu;    // Coefficient for waktu
+    float _intercept;    // Intercept of the model
+    float _learningRate; // Learning rate for updating coefficients
+};
+
+#endif
